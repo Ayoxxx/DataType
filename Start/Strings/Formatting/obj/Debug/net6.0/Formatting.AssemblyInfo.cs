ba0f3f8987676cd1b6924167142d0296b4a5345e
Ayoxxx <fawasyusuf31@gmail.com>
2025-07-08 17:04:35 +0100

    Formatting

--- Start/Strings/Formatting/obj/Debug/net6.0/Formatting.AssemblyInfo.cs
+++ Start/Strings/Formatting/obj/Debug/net6.0/Formatting.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Formatting")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6385b1b1aa08c087d143a4dec109a07aa77cbfe1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7a2cd33074c77d6820631302c710ff62b05aa2f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Formatting")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Formatting")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
