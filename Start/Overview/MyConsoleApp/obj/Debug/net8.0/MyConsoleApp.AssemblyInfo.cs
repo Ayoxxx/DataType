986d8187aa860242f2e23581353cdfa582a7e156
Ayoxxx <fawasyusuf31@gmail.com>
2025-07-17 00:47:48 +0100

    Formatting Date

--- Start/Overview/MyConsoleApp/obj/Debug/net8.0/MyConsoleApp.AssemblyInfo.cs
+++ Start/Overview/MyConsoleApp/obj/Debug/net8.0/MyConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2766dba702a149557be58936cf82918b2244d3d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd465c82c983ddb2616deeba67aa3c3b6527cd69")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
